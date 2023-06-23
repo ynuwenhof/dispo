16756f33e1549b2758631a8fafe61be8c4842585
Yannic Nuwenhof <ynuwenhof@pm.me>
2023-06-23 15:49:10 +0200

    Add exported crate functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+use std::str::FromStr;
+use email_address::EmailAddress;
+
 mod bloom;
 
 include!(concat!(env!("OUT_DIR"), "/dispo.rs"));
+
+pub fn is_valid(email: &str) -> bool {
+    EmailAddress::from_str(email).map_or(false, |e| is_valid_domain(e.domain()))
+}
+
+pub fn is_valid_domain(domain: &str) -> bool {
+    !BLOOM.contains(&domain)
+}
